9b3d01563ebfa39d74c9ebb0427f56847c8537ff
atheo810 <atheos810@gmail.com>
2024-03-18 21:41:43 +0800

    feat: edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,15 +2,17 @@
 @section('title')
 	Edit todo
 @endsection
-@section('content`')
-	<form action="" method="post" class="mt-4 p-4">
+@section('content')
+	<form action="{{ route('update', $todo[0]->id) }}" method="post" class="mt-4 p-4">
+		@csrf
+		@method('PUT')
 		<div class="form-group m-3">
 			<label for="name">Todo name here</label>
-			<input type="text" name="name" class="form-control">
+			<input type="text" name="name" class="form-control" value="{{ $todo[0]->name }}" autocomplete="off">
 		</div>
 		<div class="form-group m-3">
 			<label for="description">Todo description here</label>
-			<textarea type="text" name="description" class="form-control">Description here</textarea>
+			<textarea type="text" name="description" class="form-control">{{ $todo[0]->description }}</textarea>
 		</div>
 		<div class="form-group m-3">
 			<input type="submit" value="update", class="btn btn-primary float-end">
