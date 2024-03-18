2ee8b842d083c968aaf779256dfd12ea1559abe7
atheo810 <atheos810@gmail.com>
2024-03-18 21:41:05 +0800

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TodoController::class, 'index']);
-Route::get('/create', [TodoController::class, 'create']);
-Route::get('/details/{id}', [TodoController::class, 'details']);
-Route::get('/edit/{id}', [TodoController::class, 'edit']);
-Route::put('/update/{id}', [TodoController::class, 'update']);
-Route::delete('/delete/{id}', [TodoController::class, 'delete']);
+Route::get('/', [TodoController::class, 'index'])->name('index');
+Route::get('/create', [TodoController::class, 'create'])->name('create');
+Route::post('/store', [TodoController::class, 'store'])->name('store');
+Route::get('/details/{id}', [TodoController::class, 'details'])->name('details');
+Route::get('/edit/{id}', [TodoController::class, 'edit'])->name('edit');
+Route::put('/update/{id}', [TodoController::class, 'update'])->name('update');
+Route::delete('/delete/{id}', [TodoController::class, 'delete'])->name('delete');
