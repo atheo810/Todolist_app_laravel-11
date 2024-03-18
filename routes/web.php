4c40b1104116135e77b9f6ea539b04b57e76f912
atheo810 <atheos810@gmail.com>
2024-03-19 00:12:10 +0800

    feat: route register & login

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\LoginMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TodoController::class, 'index'])->name('index');
-Route::get('/create', [TodoController::class, 'create'])->name('create');
-Route::post('/store', [TodoController::class, 'store'])->name('store');
-Route::get('/details/{id}', [TodoController::class, 'details'])->name('details');
-Route::get('/edit/{id}', [TodoController::class, 'edit'])->name('edit');
-Route::put('/update/{id}', [TodoController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [TodoController::class, 'delete'])->name('delete');
+
+Route::middleware([LoginMiddleware::class])->group(function () {
+    Route::get('/', [TodoController::class, 'index'])->name('index');
+    Route::get('/create', [TodoController::class, 'create'])->name('create');
+    Route::post('/store', [TodoController::class, 'store'])->name('store');
+    Route::get('/details/{id}', [TodoController::class, 'details'])->name('details');
+    Route::get('/edit/{id}', [TodoController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [TodoController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [TodoController::class, 'delete'])->name('delete');
+});
+
+Route::get('/login', [UserController::class, 'login'])->name('login');
+Route::get('/register', [UserController::class, 'register'])->name('register');
