60504078ac97c1d32d2eae0129374d8ce576c5ee
atheo810 <atheos810@gmail.com>
2024-03-18 21:49:48 +0800

    fix: link no task available

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
 		<div class="col-12 align-self-center">
 			<ul class="list-group">
 				@if ($todos->count() === 0)
-					<li class="list-group-item"><a href="">No Todo Available</a></li>
+					<li class="list-group-item"><span>No Todo Available</span></li>
 				@else
 					@foreach ($todos as $todo)
 						<li class="list-group-item"><a href="{{ route('details', $todo->id) }}">{{ $todo->name }}</a></li>
