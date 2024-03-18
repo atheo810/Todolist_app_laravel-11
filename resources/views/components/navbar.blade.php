d02aba4e2e0f34fc2637128abf732c9091caf981
atheo810 <atheos810@gmail.com>
2024-03-19 00:12:25 +0800

    feat: navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light bg-light">
+	<div class="container">
+		<a href="/" class="link-underline link-underline-opacity-0"><span class="navbar-brand mb-0 h1">Todo</span></a>
+		<a href="/create"><span class="btn btn-primary">Create Todo</span></a>
+	</div>
+</nav>
