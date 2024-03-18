c33663eafb9ec3a70954c040939a5d3f26730695
atheo810 <atheos810@gmail.com>
2024-03-18 16:03:55 +0800

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/create', function () {
-    return view('create');
-});
+Route::get('/', [TodoController::class, 'index']);
+Route::get('/create', [TodoController::class, 'create']);
+Route::get('/details/{id}', [TodoController::class, 'details']);
+Route::get('/edit/{id}', [TodoController::class, 'edit']);
+Route::put('/update/{id}', [TodoController::class, 'update']);
+Route::delete('/delete/{id}', [TodoController::class, 'delete']);
