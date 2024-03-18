55bedf3dacbeff4a5e4e88114a1f49a72c9c2bf7
atheo810 <atheos810@gmail.com>
2024-03-18 14:52:39 +0800

    feat: dashboard menu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title')
+	My todo App
+@endsection
+@section('content')
+	<div class="row mt-3">
+		<div class="col-12 align-self-center">
+			<ul class="list-group">
+				<li class="list-group-item"><a href="details">Dummy todo here</a></li>
+				<li class="list-group-item"><a href="details">Dummy todo here</a></li>
+				<li class="list-group-item"><a href="details">Dummy todo here</a></li>
+				<li class="list-group-item"><a href="details">Dummy todo here</a></li>
+				<li class="list-group-item"><a href="details">Dummy todo here</a></li>
+			</ul>
+		</div>
+	</div>
+@endsection
