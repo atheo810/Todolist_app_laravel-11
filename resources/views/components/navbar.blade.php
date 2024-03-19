48dc9d50ca3cf0e5e0c3a80aaa545c64d19937c0
atheo810 <atheos810@gmail.com>
2024-03-19 14:01:07 +0800

    feat: logout button

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,15 @@
-<nav class="navbar navbar-light bg-light">
-	<div class="container">
-		<a href="/" class="link-underline link-underline-opacity-0"><span class="navbar-brand mb-0 h1">Todo</span></a>
-		<a href="/create"><span class="btn btn-primary">Create Todo</span></a>
+<nav class="navbar bg-body-tertiary">
+	<div class="container-fluid">
+		<div class="navbar-brand mb-0 h1">
+			<a href="/" class="link-underline link-underline-opacity-0"><span class="navbar-brand mb-0 h1">Todo App</span></a>
+		</div>
+		<div class="d-flex">
+			<a href="/create"><span class="btn btn-primary me-3">Create Todo</span></a>
+			<form action="{{ route('logout') }}" method="post">
+				@csrf
+				<button type="submit" name="submit" id="submit" class="btn btn-primary">Logout</button>
+			</form>
+		</div>
+
 	</div>
 </nav>
